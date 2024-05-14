47b6a5d4a779644479624d1330533940b6b92d5e
exe123deadstart <valencia.exequiel123@gmail.com>
2024-05-14 23:07:22 +0900

    metadata fixed title and favicon

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import iconLogo from '@/assets/logo.svg'
+
+document.title = `ZikStudios`;
+const link = document.querySelector("[rel='icon']")
+link?.setAttribute('href',iconLogo)
+    
 
 const app = createApp(App)
 
